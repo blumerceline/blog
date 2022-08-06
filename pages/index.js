f5c9743649fec146f6f05f51a239ba68171306e0
blumerceline <blumerceline@gmail.com>
2022-08-06 13:57:13 +1000

    Changed index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+
 import { PostCard, Categories, PostWidget } from '../components';
 import Head from 'next/head'
 import Image from 'next/image'
@@ -6,7 +6,7 @@ import {getPosts} from '../services'
 import { Key } from 'react';
 
 
-export default function Home({posts}:{posts:any}){
+export default function Home({posts}){
     return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -15,7 +15,9 @@ export default function Home({posts}:{posts:any}){
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='col-span-1 lg:col-span-8'>
-          {posts.map((post: { node: any; title: Key | null | undefined; }) => <PostCard post={post.node} key={post.title} />)}
+        {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
         <div className='col-span-1 lg:col-span-4'>
           <div className='lg:sticky relative top-8'>
