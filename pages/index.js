17cef3093729f3154fc242d116e4ff313328f8a2
blumerceline <blumerceline@gmail.com>
2022-08-08 21:27:09 +1000

    changes

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,12 @@ export default function Home({posts}){
     return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>My Blog</title>
+        <title>Celine Blumer | Brisbane Based Web Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='col-span-1 lg:col-span-8'>
-        {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
+
         </div>
         <div className='col-span-1 lg:col-span-4'>
           <div className='lg:sticky relative top-8'>
