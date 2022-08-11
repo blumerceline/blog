e6425505e61d6ef686a5c36b35d0e25b011897ef
blumerceline <blumerceline@gmail.com>
2022-08-11 20:26:50 +1000

    wdw

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
 
 import { PostCard, Categories, PostWidget } from '../components';
 import Head from 'next/head'
-import Image from 'next/image'
 import {getPosts} from '../services'
 import { Key } from 'react';
+import Image from 'next/image';
+import img from '../public/bg.jpg';
 
-const BgImage = dynamic(() => import("../public/bg.jpg"),{
-  ssr: false
-});
 
 export default function Home({posts}){
     return (
     <div className="container mx-auto px-10 mb-8 ">
+      <Image src={img} width="280" height="280"/>
       <div className="bg-fixed">
         <Head>
           <title>Celine Blumer | Brisbane Based Web Developer</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <BgImage/>
         
         <h1 className='text-lg text-black heading-text'>Web dev,</h1>
         <h1 className='text-lg text-black heading-text'>made simple!</h1>
